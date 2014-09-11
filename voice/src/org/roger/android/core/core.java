022d8c6704da088cf0bac820993541d2c0eb0edc
iamroger <imiracle@live.cn>
2014-09-11 19:21:48 +0800

    incoming tone

--- voice/src/org/roger/android/core/core.java
+++ voice/src/org/roger/android/core/core.java
@@ -1,24 +1,3 @@
-/* 
- * Copyright (C) 2009 Jurij Smakov <jurij@wooyd.org>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 
 package org.roger.android.core;
 
@@ -49,7 +28,16 @@ public class core {
     	Log.e("Droid", "test" );
     }
     public int receive( String remoteCall ){
-    	ctx.incomingCall( remoteCall.substring(5,remoteCall.indexOf('@')) );
+    	if( remoteCall.equals("#disconnected#") ) {
+    		if( droid.self != null )
+				droid.self.startActivity("main");
+    	}else if ( remoteCall.equals("#confirmed#") ) {
+    		if( droid.self != null )
+    			droid.self.startActivity("calling", "route", "out" );
+    	}else {
+    		ctx.incomingCall( remoteCall.substring(5,remoteCall.indexOf('@')) );
+    	}
     	return 1;
+    	
     }
 }
